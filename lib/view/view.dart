640ee1f7e172e1f62ad18e32293a48ac05da7a9d
ulngollm <ulngollm@gmail.com>
2021-02-16 00:43:36 +0300

    working loader and refresh

--- lib/view/view.dart
+++ lib/view/view.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:http_test/model/cat.dart';
 import 'package:http_test/view/view_model.dart';
 import 'package:http_test/widget/card.dart';
+import 'package:http_test/widget/catcard.dart';
 import 'package:pull_to_refresh/pull_to_refresh.dart';
 import 'package:stacked/stacked.dart';
 
@@ -10,25 +10,29 @@ class View extends StatelessWidget {
   Widget build(BuildContext context) {
     return ViewModelBuilder<ViewModel>.reactive(
       builder: (context, model, _) => Scaffold(
-        body: Container(
-          child: SmartRefresher(
-            controller: model.refreshController,
-            header: ClassicHeader(),
-            enablePullUp: true,
-            onRefresh: model.fetch,
-            onLoading: model.fetch,
-            child: Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 25),
-              child: ListView(
-                children: List.generate(model.cats?.length ?? 0,
-                    (index) => CatCard.fromObject(model.cats[index])),
+        body: model.isBusy
+            ? Center(child: CircularProgressIndicator())
+            : Container(
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 25),
+                  child: SmartRefresher(
+                    controller: model.refreshController,
+                    header: ClassicHeader(),
+                    onRefresh: model.fetch,
+                    child: ListView(
+                      children: List.generate(model.cats?.length ?? 0,
+                          (index) => CatCard.fromObject(model.cats[index])),
+                    ),
+                  ),
+                ),
               ),
-            ),
-          ),
-        ),
       ),
       viewModelBuilder: () {
         return ViewModel();
+      },
+      onModelReady: (model) {
+        print(model.isBusy);
+        model.fetch();
       },
     );
   }
--- lib/view/view_model.dart
+++ lib/view/view_model.dart
@@ -1,16 +1,13 @@
-import 'package:flutter/material.dart';
 import 'package:http_test/api/catapi.dart';
 import 'package:http_test/model/cat.dart';
-import 'package:http_test/widget/card.dart';
 import 'package:pull_to_refresh/pull_to_refresh.dart';
 import 'package:stacked/stacked.dart';
 
 class ViewModel extends BaseViewModel {
   ViewModel();
   List<Cat> cats;
-
-  RefreshController refreshController =
-      RefreshController(initialRefresh: false);
+  RefreshController refreshController = RefreshController(
+      initialRefresh: false, initialRefreshStatus: RefreshStatus.refreshing);
 
   Future<List<Cat>> getCats() async {
     final result = await CatApi.getCatsList();
@@ -23,7 +20,7 @@ class ViewModel extends BaseViewModel {
   Future<void> fetch() async {
     setBusy(true);
     cats = await getCats();
-    setBusy(false);
     refreshController.refreshCompleted();
+    setBusy(false);
   }
 }
